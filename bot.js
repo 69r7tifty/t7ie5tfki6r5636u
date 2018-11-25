620d539b2155e888c68fc81fbc534fc8abd7eaba
69r7tifty <45335952+69r7tifty@users.noreply.github.com>
2018-11-25 19:14:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,36 @@
-{
-  "الاسم": "bytetech" ،
-  "الإصدار": "2.1.0" ،
-  "description": "- AWMOMESS - -"
-  "main": "bot.js" ،
-  "scripts": {
-    "start": "node bot.js"
-  }،
-  "التبعيات": {
-    "ascii-data-table": "^ 2.1.1" ،
-    "canvas": "^ 1.6.10" ،
-    "dateformat": "^ 3.0.3" ،
-    "discord.js": "^ 11.3.2" ،
-    "صريح": "^ 4.16.3" ،
-    "ffmpeg": "0.0.4" ،
-    "ffmpeg-binaries": "^ 3.2.2-3" ،
-    "fs": "0.0.2" ،
-    "get-youtube-id": "^ 1.0.0" ،
-    "jimp": "^ 0.2.28" ،
-    "لحظة": "^ 2.22.1" ،
-    "ms": "^ 2.1.1" ،
-    "opusscript": "0.0.6" ،
-    "quick.db": "^ 6.2.0" ،
-    "request": "^ 2.85.0" ،
-    "simple-youtube-api": "^ 5.0.2" ،
-    "snekfetch": "^ 3.6.4" ،
-    "sqlite": "^ 2.9.1" ،
-    "sqlite3": "^ 4.0.0" ،
-    "youtube-info": "^ 1.2.0" ،
-    "ytdl-core": "^ 0.20.3"
-  }،
-  "المحركات": {
-    "عقدة": "8.4.0"
-  }
-}
+
+const discord = تتطلب ('discord.js')؛
+const client = new Discord.Client ()؛
+const prefix = 'البرفيكس حق البوت'
+ 
+ 
+ 
+client.on ("ready"، () => {
+  console.log (`Logged in as $ {client.user.tag}!`)؛
+client.user.setGame (`LastCodes4Ever`، "https://www.twitch.tv/dggamingbot")
+  console.log ( '')
+  console.log ( '')
+  console.log ( '╔ [═══════════════════════════════════════════ ══════════════════════] ╗ ')
+  console.log (`[Start] $ {new Date ()}`)؛
+  console.log ( '╚ [═══════════════════════════════════════════ ══════════════════════] ╝ ')
+  console.log ( '')
+  console.log ( '╔ [════════════════════════════════════] ╗')؛
+  console.log (`تسجيل الدخول كـ * [" $ {client.user.username} "]`)؛
+  console.log ( '')
+  console.log ('معلومات:')
+  console.log ( '')
+  console.log (`خوادم! [" $ {client.guilds.size} "]`)؛
+  console.log (`Users! [" $ {client.users.size} "]`)؛
+  console.log (`channels! [" $ {client.channels.size} "]`)؛
+  console.log ( '╚ [════════════════════════════════════] ╝')
+  console.log ( '')
+  console.log ( '╔ [════════════] ╗')
+  console.log ('Bot Is Online')
+  console.log ( '╚ [════════════] ╝')
+  console.log ( '')
+  console.log ( '')
+})؛
+ 
+ 
+ 
+client.login (process.env.BOT_TOKEN)؛
